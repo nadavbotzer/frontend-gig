d75337eddc8db0210bfd0267a69f1499552150ba
Bar rabin <barrabin2000@gamil.com>
2025-02-13 20:52:32 +0200

    Add MakeIt cmp

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,17 @@
+import { Category } from '../cmps/Category';
 import { Hero } from '../cmps/Hero';
 import { HomeVideo } from '../cmps/HomeVideo';
+import { MakeIt } from '../cmps/MakeIt';
+
 
 
 export function HomePage() {
     return (
         <section className='home-page'>
             <Hero />
+            <Category />
             <HomeVideo />
+            <MakeIt />
 
 
 
