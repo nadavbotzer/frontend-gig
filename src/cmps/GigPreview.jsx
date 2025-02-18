b9396390aaa9976ca5c05fb60e7293bd688003c4
Nadav Botzer <botzer14@gmail.com>
2025-02-18 20:57:19 +0200

    Adjust gig preview

--- src/cmps/GigPreview.jsx
+++ src/cmps/GigPreview.jsx
@@ -1,26 +1,55 @@
 import { Link } from 'react-router-dom'
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import StarIcon from '@mui/icons-material/Star';
-import { ImgCarousel } from './ImgCarousel';
+import { useDispatch } from 'react-redux'
+import { updateGig } from '../store/actions/gig.actions'
+import { userService } from '../services/user'
+import { ImgCarousel } from '../cmps/ImgCarousel'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
+import StarIcon from '@mui/icons-material/Star'
 
 export function GigPreview({ gig, goToDetails }) {
-    return <article className="preview">
-        <div className="img-wrapper">
-            <ImgCarousel imgUrls={gig.imgUrls} onClickImg={goToDetails} />
+    const dispatch = useDispatch()
 
-            <button className="like-btn"><FavoriteBorderIcon /></button>
-        </div>
-        <div className="gig-user-preview">
-            <img className="gig-user-img" src={gig.owner.imgUrl} />
-            {gig.promoted && <span>Ad by </span>}
-            <span className="gig-user-fullname"> {gig.owner.fullname}</span>
-        </div>
-        <Link className="gig-title" to={`/gig/${gig._id}`}>{gig.title}</Link>
-        <div className="gig-rating-preview">
-            <span><StarIcon /></span>
-            <span >4.9 </span>
-            <span className="gig-previews-count">({gig.reviews.length})</span>
-        </div>
-        <p className="gig-price">From USD${gig.price}</p>
-    </article>
+    function onClickLike() {
+        console.log('gig liked')
+
+        const loggedInUser = userService.getLoggedinUser()
+        if (!loggedInUser) return // Ensure the user is logged in
+
+        // Clone the gig object and add the logged-in user to the likedByUsers array
+        const updatedGig = {
+            ...gig,
+            likedByUsers: [...gig.likedByUsers, loggedInUser]  // Add the user to likedByUsers
+        }
+
+        // Dispatch the action to update the gig in the store
+        dispatch(updateGig(updatedGig))  // Dispatching the action will update the store
+    }
+
+    function getGigReviewsAvgRate(reviews) {
+        if (reviews.length === 0) return 0
+        const totalRate = reviews.reduce((sum, review) => sum + review.rate, 0)
+        const avgRate = totalRate / reviews.length
+        return avgRate.toFixed(1)
+    }
+
+    return (
+        <article className="preview">
+            <div className="img-wrapper">
+                <ImgCarousel imgUrls={gig.imgUrls} onClickImg={goToDetails} />
+                <button className="like-btn" onClick={onClickLike}><FavoriteBorderIcon /></button>
+            </div>
+            <div className="gig-user-preview">
+                <img className="gig-user-img" src={gig.owner.imgUrl} />
+                {gig.promoted && <span>Ad by </span>}
+                <span className="gig-user-fullname"> {gig.owner.fullname}</span>
+            </div>
+            <Link className="gig-title" to={`/gig/${gig._id}`}>{gig.title}</Link>
+            <div className="gig-rating-preview">
+                <span><StarIcon /></span>
+                <span>{getGigReviewsAvgRate(gig.reviews)}</span>
+                <span className="gig-previews-count">({gig.reviews.length})</span>
+            </div>
+            <p className="gig-price">From USD${gig.price}</p>
+        </article>
+    )
 }
--- src/services/gig/index.js
+++ src/services/gig/index.js
@@ -25,24 +25,68 @@ function getEmptyGig() {
             rate: getRandomIntInclusive(1, 5),
         },
         daysToMake: getRandomIntInclusive(1, 10),
-        description: ['A professional service to ' + getRandomGigTitle().toLowerCase() + '.',
-        'A professional service to ' + getRandomGigTitle().toLowerCase() + '.',
-        'A professional service to ' + getRandomGigTitle().toLowerCase() + '.',
-        'A professional service to ' + getRandomGigTitle().toLowerCase() + '.',
-        'A professional service to ' + getRandomGigTitle().toLowerCase() + '.',
-        'A professional service to ' + getRandomGigTitle().toLowerCase() + '.',
-        'A professional service to ' + getRandomGigTitle().toLowerCase() + '.'
+        description: [
+            'A professional service to ' + getRandomGigTitle().toLowerCase() + '.',
+            'A professional service to ' + getRandomGigTitle().toLowerCase() + '.',
+            'A professional service to ' + getRandomGigTitle().toLowerCase() + '.',
+            'A professional service to ' + getRandomGigTitle().toLowerCase() + '.',
+            'A professional service to ' + getRandomGigTitle().toLowerCase() + '.',
+            'A professional service to ' + getRandomGigTitle().toLowerCase() + '.',
+            'A professional service to ' + getRandomGigTitle().toLowerCase() + '.'
         ],
         avgResponseTime: getRandomIntInclusive(1, 24),
         loc: getRandomLocation(),
-        imgUrls: ["https://fiverr-res.cloudinary.com/t_gig_cards_web,q_auto,f_auto/gigs/193068192/original/a170671ee931142c619a392cd06db59b9a60eec7.jpg"
-            , "https://fiverr-res.cloudinary.com/t_gig_cards_web,q_auto,f_auto/gigs/193068192/original/a170671ee931142c619a392cd06db59b9a60eec7.jpg",
+        imgUrls: [
+            "https://fiverr-res.cloudinary.com/t_gig_cards_web,q_auto,f_auto/gigs/193068192/original/a170671ee931142c619a392cd06db59b9a60eec7.jpg",
+            "https://fiverr-res.cloudinary.com/t_gig_cards_web,q_auto,f_auto/gigs/193068192/original/a170671ee931142c619a392cd06db59b9a60eec7.jpg",
             "https://fiverr-res.cloudinary.com/t_gig_cards_web,q_auto,f_auto/gigs/193068192/original/a170671ee931142c619a392cd06db59b9a60eec7.jpg",
             "https://fiverr-res.cloudinary.com/t_gig_cards_web,q_auto,f_auto/gigs/193068192/original/a170671ee931142c619a392cd06db59b9a60eec7.jpg"
         ],
         tags: getRandomTags(),
         likedByUsers: [],
-        reviews: [],
+        reviews: [
+            {
+                name: "rachelrbarnes1",
+                country: "United States",
+                flag: "https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png",
+                review: "Incredibly grateful for the amazing experience working with you . You are so talented and a kind soul! I highly recommend if you want high quality art to work with her every time",
+                reviewedAt: "Published 1 month ago",
+                rate: getRandomIntInclusive(1, 5)
+            },
+            {
+                name: "mark001994",
+                country: "Austria",
+                flag: "https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1e6-1f1f9.png",
+                review: "The artist was very kind and polite also very fast at the communication. The delivery of the project was on time. And her work is worth the money. I'm really excited about the painting she did. I can truely recommend the Aritst and her work. Big Thanks! :)",
+                reviewedAt: "Published 1 month ago",
+                rate: getRandomIntInclusive(1, 5)
+            },
+            {
+                name: "thurstonrobby",
+                country: "United States",
+                flag: "https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png",
+                review: "incredible on how precise that art is, picture perfect. 100% amazing job and I will use your services again ...",
+                reviewedAt: "Published 3 weeks ago",
+                rate: getRandomIntInclusive(1, 5)
+            },
+            {
+                name: "gavrielm",
+                country: "Israel",
+                flag: "https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1ee-1f1f1.png",
+                review: "amazing saller and great work",
+                reviewedAt: "Published 6 days ago",
+                rate: getRandomIntInclusive(1, 5)
+            },
+            {
+                name: "garebear52",
+                country: "United States",
+                flag: "https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png",
+                review: "Beautiful drawing! Just what I wanted.",
+                reviewedAt: "Published 1 week ago",
+                rate: getRandomIntInclusive(1, 5)
+            }
+        ]
+        ,
     }
 }
 
