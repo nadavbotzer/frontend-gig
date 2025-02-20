db7c2f3d7d9b486805e7ab45ba6c0dfe3b38909c
Bar rabin <barrabin2000@gamil.com>
2025-02-20 12:24:02 +0200

    Add css for header

--- src/pages/GigDetails.jsx
+++ src/pages/GigDetails.jsx
@@ -27,7 +27,7 @@ export function GigDetails() {
   }, [gig])
 
   async function loadGig(gigId) {
-    setIsLoading(true);
+    setIsLoading(true)
     try {
       const gig = await gigService.getById(gigId)
       setGig(gig)
